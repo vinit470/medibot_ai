f782af721bbdc1d65396ab90e08e7f5e4b56569f
Vinit <vinitkumar88930@gmail.com>
2026-04-04 16:05:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="vinit kumar",
+    author_email="vinitkumar88930@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
