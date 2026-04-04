90729024253c090c84ba0247382788cc78088cf8
Vinit <vinitkumar88930@gmail.com>
2026-04-04 11:12:50 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating diractory
+
+mkdir -p src
+mkdir -p research
+
+# creating files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully."
